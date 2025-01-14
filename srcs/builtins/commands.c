8b9ea11d6a3018143f557a52201ed3209883d1d6
Mickael Chen <micchen@c1r1p8.42lehavre.fr>
2025-01-14 03:39:11 +0100

    small change

--- srcs/builtins/commands.c
+++ srcs/builtins/commands.c
@@ -93,7 +93,7 @@ void	commands(t_shell *shell)
 {
 	char    path[PATH_MAX];
 	if (ft_strcmp(shell->cmd[0], "pwd") == 0)
-		printf("%s\n", getcwd(path, sizeof(path)));
+		ft_pwd(&shell->cmd[1]);
 	else if (ft_strcmp(shell->cmd[0], "env") == 0)
 		print_list(shell->env);
 	else if (ft_strcmp(shell->cmd[0], "export") == 0)
@@ -103,7 +103,9 @@ void	commands(t_shell *shell)
 	else if (ft_strcmp(shell->cmd[0], "unset") == 0)
 		ft_unset(shell->env, shell->cmd + 1);
 	else if (ft_strcmp(shell->cmd[0], "exit") == 0)
-		ft_exit(shell, shell->cmd+1);
+		ft_exit(shell, shell->cmd + 1);
+	else if (ft_strcmp(shell->cmd[0], "cd") == 0)
+		ft_cd(shell, shell->cmd[1]);
 	else
 		 external_command(shell, shell->env);
 		//printf("command not found: %s\n", shell->cmd[0]);
